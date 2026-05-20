89a6fccbd5e448b80b1efd88c1619656acd63e38
joaquingomez2007 <joaquingomezjg8@gmail.com>
2026-05-20 16:23:02 -0300

    agregamos primera actividad que es consigna 1 act 6 en la cual implementamos una funcion en python que evalua la expresion
    Added sets A, B, and C, and evaluated critical users based on their membership.

--- consigna1act6.py
+++ consigna1act6.py
@@ -1,7 +1,12 @@
-def evaluar_expresion(p, q, r):
-    return (p or q) and r
+A = {101, 102, 103, 104, 105, 106}
+B = {104, 105, 106, 107, 108}
+C = {102, 105, 109}
 
-print(evaluar_expresion(True, True, True))   
-print(evaluar_expresion(True, False, False))  
-print(evaluar_expresion(False, True, True))  
-print(evaluar_expresion(False, False, True))  
+universo = A | B | C
+
+for uid in universo:
+    p = uid in A
+    q = uid in B
+    r = uid in C
+    es_critico = (p or q) and r
+    print("Usuario:", uid, "-> Critico:", es_critico)
