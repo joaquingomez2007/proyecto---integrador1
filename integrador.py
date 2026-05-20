c385c45a42d07744d06fb7b76c38b844d4b3c9e4
matias pajon <matipajoncito@gmail.com>
2026-05-20 10:13:10 -0300

    Se crea una funcion que verifica usuarios criticos usando condiciones booleanas y se prueban distintos casos con print

--- integrador.py
+++ integrador.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def usuario_critico(p, q, r):
     return (p or q) and r
 
@@ -7,3 +8,12 @@ print(usuario_critico(True, True, True))    # True
 print(usuario_critico(True, False, True))   # True
 print(usuario_critico(False, True, False))  # False
 print(usuario_critico(False, False, True))  # False
+=======
+def evaluar_expresion(p, q, r):
+    return (p or q) and r
+
+print(evaluar_expresion(True, True, True))   
+print(evaluar_expresion(True, False, False))  
+print(evaluar_expresion(False, True, True))  
+print(evaluar_expresion(False, False, True))  
+>>>>>>> ad58f13fdeb33d7cb6543fd37de078c54595c4ea
