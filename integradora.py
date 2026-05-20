0f8c0d034d58f993bed594f4fed9c4481d431c33
Benjamin Lucero <lucerocaraffaben@gmai.com>
2026-05-20 17:08:53 -0300

    consigna 1, Actividad 6, Benjamin Lucero

---
+++ integradora.py
@@ -0,0 +1,12 @@
+A = {101, 102, 103, 104, 105, 106}
+B = {104,105, 106,107,108}
+C = {102,105,109}
+
+universo = A|B|C 
+
+for uid in universo:
+    p = uid in A
+    q = uid in B
+    r = uid in C
+    es_critico = (p or q) and r
+    print("Usuario:", uid, "-> Critico:", es_critico)
