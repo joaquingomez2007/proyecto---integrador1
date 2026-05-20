8d9253b2b16df4957a2f36e439fe32404426d942
joaquingomez2007 <joaquingomezjg8@gmail.com>
2026-05-20 10:28:15 -0300

    Delete usuario_contraseña.py

--- usuario_contraseña.py
+++
@@ -1,7 +0,0 @@
-usuario= input("Ingrese su usuario: ")
-contraseña= input("Ingrese su contraseña: ")
-if usuario == "matias" and contraseña == "123456":
-    print("...Bienvenido al sistema... matias")
-else:
-    print("Usuario o contraseña incorrectos")
-    
