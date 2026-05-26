9f7b4e3ef55a581d7ae8dd202d57af10762b6c00
joaquingomez <joaquingomezjg8@gmail.com>
2026-05-26 11:59:49 -0300

    actualizo cambios consigna3act3B

---
+++ consigna3act3B.py
@@ -0,0 +1,54 @@
+def cargar_matriz():
+    matriz_local = []
+    for servidor in range(2):
+        fila_servidor = []
+        for funcion in range(3):
+            num = int(input("Ingrese un numero: "))
+            fila_servidor.append(num)
+        matriz_local.append(fila_servidor)
+    return matriz_local
+
+def mostrar_matriz(M):
+    print("Matriz:")
+    for fila in M:
+        print(fila)
+
+def promedio_por_funcion(M):
+    print("\nPromedio por funcion:")
+
+    for funcion in range(3):
+        suma = 0
+    
+        for servidor in range(2):
+            suma = suma + M[servidor][funcion]
+        
+        promedio = suma / 2
+        print(promedio)
+
+def promedio_por_servidor(M):
+    print("\nPromedio por servidor:")
+
+    for servidor in range(2):
+        suma = 0
+
+        for funcion in range(3):
+            suma = suma + M[servidor][funcion]
+            
+        promedio = suma / 3
+        print(promedio)
+
+
+
+def mostrar_transpuesta(M):
+    print("\nMatriz transpuesta:")
+    
+    for funcion in range(3):
+        for servidor in range(2):
+            print(M[servidor][funcion], end=" ")
+        print()
+
+M = cargar_matriz()
+mostrar_matriz(M)
+promedio_por_funcion(M)
+promedio_por_servidor(M)
+mostrar_transpuesta(M)
