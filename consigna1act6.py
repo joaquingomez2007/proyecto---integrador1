e645bbae228a6ed926aa0125aaec581f321c151a
joaquingomez <joaquingomezjg8@gmail.com>
2026-05-26 11:52:40 -0300

    correccion consigna 1 actividad 6 simplificamos el programa para que el entendimiento nos sea mas facil en la actividad lo que realizamos es implementamos una funcion en python que evalua la expresion

--- consigna1act6.py
+++ consigna1act6.py
@@ -1,12 +1,17 @@
-A = {101, 102, 103, 104, 105, 106}
-B = {104, 105, 106, 107, 108}
-C = {102, 105, 109}
+a = [101, 102, 103, 104, 105, 106]
+b = [104, 105, 106, 107, 108]
+c = [102, 105, 109]
 
-universo = A | B | C
+todos = [101, 102, 103, 104, 105, 106, 107, 108, 109]
 
-for uid in universo:
-    p = uid in A
-    q = uid in B
-    r = uid in C
-    es_critico = (p or q) and r
-    print("Usuario:", uid, "-> Critico:", es_critico)
+for uid in todos:
+    print("Usuario:", uid)
+    
+    if uid in a:
+        print("- Esta en el Grupo P")
+        
+    if uid in b:
+        print("- Esta en el Grupo Q")
+        
+    if uid in c:
+        print("- Esta en el Grupo R")
