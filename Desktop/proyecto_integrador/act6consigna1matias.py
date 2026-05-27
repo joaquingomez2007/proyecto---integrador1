32c3e995fbf8bbde5088c01d0ce8019dec6d6736
matias <matipajoncito@gmail.com>
2026-05-27 17:05:44 -0400

    Refactor usuario_critico and add user group checks

--- Desktop/proyecto_integrador/act6consigna1matias.py
+++ Desktop/proyecto_integrador/act6consigna1matias.py
@@ -1,8 +1,33 @@
-def usuario_critico(p, q, r):
-    return (p or q) and r
+a = [101, 102, 103, 104, 105, 106]
+b = [104, 105, 106, 107, 108]
+c = [102, 105, 109]
 
-print(usuario_critico(True, True, True))  
-print(usuario_critico(True, False, True))   
-print(usuario_critico(False, True, False))  
-print(usuario_critico(False, False, True))  
+todos = [101, 102, 103, 104, 105, 106, 107, 108, 109]
 
+for uid in todos:
+    print("Usuario:", uid)
+    
+    if uid in a:
+        print("- Esta en el Grupo P")
+        
+    if uid in b:
+        print("- Esta en el Grupo Q")
+        
+    if uid in c:
+        print("- Esta en el Grupo R")
+
+print("\nUsuarios criticos:")
+
+for uid in todos:
+
+    es_critico = ((uid in a) or (uid in b)) and (uid in c)
+
+    if es_critico:
+        print(uid)
+
+print("\nUsuarios no criticos:")
+
+for uid in todos:
+    no_critico = ((uid in c)) and not ((uid in a) or (uid in b))
+    if no_critico:
+        print(uid)
