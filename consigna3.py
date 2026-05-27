56773a0aa920a8e33af3fb480ebb747bffbbcf18
Valentino <valesalguero12@gmai.com>
2026-05-26 22:17:57 -0300

    se promedia fila y columma, luego se realiza matriz transpuesta

--- consigna3.py
+++ consigna3.py
@@ -1,39 +1,28 @@
-# Definimos la matriz M
-M = [
-    [120, 150, 100],  # Función 0
-    [200, 180, 220],  # Función 1
-    [90, 110, 95]     # Función 2
+m = [
+    [120, 150, 100],
+    [200, 180, 220],
+    [90,  110,  95]
 ]
 
-print("--- Tiempo promedio de ejecución por función ---")
-
+print("Tiempo promedio de ejecución por funcion")
 for i in range(3):
-    suma_funcion = 0
+    suma_fila = 0
     for j in range(3):
-        suma_funcion += M[i][j]
-        
-    promedio = suma_funcion // 3 
-    print("Función", i, ":", promedio, "ms")
+        suma_fila = suma_fila + m[i][j]
+    promedio = suma_fila // 3
+    print("Funcion", i, promedio, "ms")
 
-
-print("\n--- Tiempo promedio de ejecución por servidor ---")
-
+print("Tiempo promedio de ejecución por servidor")
 for j in range(3):
-    suma_servidor = 0
+    suma_columna = 0
     for i in range(3):
-        suma_servidor += M[i][j]
-        
-    promedio = suma_servidor // 3  
-    print("Servidor", j, ":", promedio, "ms")
+        suma_columna = suma_columna + m[i][j]
+    promedio = suma_columna // 3
+    print("Servidor", j, promedio, "ms")
 
-M_transpuesta =[
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]]
-
-for i in range(3):
-    for j in range(3):
-        M_transpuesta[j][i] = M[i][j]
-
-for fila in M_transpuesta:
-    print(fila)
+print("Matriz Transpuesta M^T:")
+for j in range(3):
+    fila_transpuesta = [0, 0, 0]
+    for i in range(3):
+        fila_transpuesta[i] = m[i][j]
+    print(fila_transpuesta)
