58562f09198718f7239ff3e87e5df4ce9f574396
BenjaminLucero <lucerocaraffaben@gmail.com>
2026-05-27 10:37:57 -0300

    agregamos si era critico o no, ya que anteriormente nos habia faltado agregarlo

--- actividad1.py
+++ actividad1.py
@@ -15,5 +15,19 @@ for uid in todos:
         
     if uid in c:
         print("- Esta en el Grupo R")
-        es_critico = (p or q) and r
-    print("Usuario:", uid, "-> Critico:", es_critico)
+
+print("\nUsuarios criticos:")
+
+for uid in todos:
+
+    es_critico = ((uid in a) or (uid in b)) and (uid in c)
+
+    if es_critico:
+        print(uid)
+
+print("\nUsuarios no criticos:")
+
+for uid in todos:
+    no_critico = ((uid in c)) and not ((uid in a) or (uid in b))
+    if no_critico:
+        print(uid)
